7905ea06954859ef9707a7abfd494367d21b3f44
fanqt <fanqt@hua-cloud.com.cn>
2020-09-30 18:03:10 +0800

    feat: 完善头部导航栏信息

--- client/view/header/index.tsx
+++ client/view/header/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
-import styles from './index.modules.less';
+import  './index.modules.less';
 const Header = () => {
-  return <div>header</div>;
+  return <div className="header_wrap">
+    <header className="header_inner">
+      <h1 className="fl">fqt</h1>
+      <nav className="fr header_title">
+        <li>
+          <a href="#">博客首页</a>
+        </li>
+        <li>
+          <a href="#">快乐生活</a>
+        </li>
+        <li>
+          <a href="#">个人介绍</a>
+        </li>
+        <li>
+          <a href="#" className="concat">联系我</a>
+        </li>
+      </nav>
+    </header>
+  </div>;
 };
 
 export default Header;
