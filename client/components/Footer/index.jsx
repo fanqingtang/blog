381cbdf7334f5c5f56c8359cea8542fc74ca594b
樊庆堂 <fanqt@hua-cloud.com.cn>
2020-11-20 18:50:15 +0800

    开发详情页面

--- client/components/Footer/index.jsx
+++ client/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import styles from './index.module.less'
 const Footer = () => {
   return (
     <footer className={styles.footer_wrap}>
-      <p>世人的悲欢并不相通，我只是觉得他们吵闹。</p>
+      <p>虽然前途不明，但是心里敞亮。加油!</p>
       <p>Copyright © Shymean 2016 - 2020蜀ICP备19012392号</p>
     </footer>
   )
